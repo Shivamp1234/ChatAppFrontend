33137dfae15caa779603640bdb6b6e723f442c85
Shivamp1234 <patilshivam156@gmail.com>
2023-05-18 14:54:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io("http://localhost:5000");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      setMessages([...messages, data]);
+    });
+  }, [messages]);
+
+  const sendmessage = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { username, message });
+    setMessage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>CHAT APP</h1>
+      <div className="app">
+        <div className="chat-window">
+          {messages.map((msg, idx) => (
+            <div>
+              {msg.username === username ? (
+                <p key={idx} className="outgoing">
+                  {username} : {msg.message}
+                </p>
+              ) : (
+                <p key={idx} className="incoming">
+                  {msg.username} : {msg.message}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+        <div>
+          <form onSubmit={sendmessage}>
+            <input
+              type="text"
+              placeholder="Enter user name..."
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <textarea
+              type="text"
+              placeholder="Enter message..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
